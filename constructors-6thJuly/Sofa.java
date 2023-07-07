4ff96d28d65ec41dadc7383250ea9a64e5009060
Sahanasonu123 <sahanad.xworkz@gmail.com>
2023-07-07 10:12:53 +0530

    constructors

---
+++ constructors-6thJuly/Sofa.java
@@ -0,0 +1,24 @@
+class Sofa{
+	
+	String colour;
+    double price;
+	String design;
+	double capacity;
+	String brand;
+	String material;
+	String purpose;
+	double weight;
+	
+	Sofa(String colour,double price,String design,double capacity,String brand,String material,String purpose,double weight)
+	{
+		System.out.println("running string,double,String,string,double,double,strinf,double const");
+		this.capacity=capacity;
+		this.brand=brand;
+		this.price=price;
+		this.colour=colour;
+		this.design=design;
+		this.material=material;
+		this.purpose=purpose;
+		this.weight=weight;
+	}
+}
---
+++ constructors-6thJuly/ToothPaste.java
@@ -0,0 +1,24 @@
+class ToothPaste{
+	
+	String colour;
+    int price;
+	int manufactureDate;
+	int capacity;
+	String brand;
+	String taste;
+	String purpose;
+	int weight;
+	
+	ToothPaste(String colour,int price,int manufactureDate,int capacity,String brand,String taste,String purpose,int weight)
+	{
+		System.out.println("running string,int,int,string,int,int,strinf,int const");
+		this.capacity=capacity;
+		this.brand=brand;
+		this.price=price;
+		this.colour=colour;
+		this.manufactureDate=manufactureDate;
+		this.taste=taste;
+		this.purpose=purpose;
+		this.weight=weight;
+	}
+}
