9d712bbd16936f7d28cd868d84f4728c82971b1a
Sahanasonu123 <sahanad.xworkz@gmail.com>
2023-08-08 14:12:05 +0530

    push

---
+++ Backup.java
@@ -0,0 +1,21 @@
+class Backup{
+	public static void main(String[] args)
+	{
+	String name;
+	int age;
+	double length;
+	
+	MyMethodName(String name,int age,double length);
+	{
+		name="abc";
+		age=20;
+		length=80;
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(length);
+	}
+	
+	
+	}
+}
+
